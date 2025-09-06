bed07621927ba474a6798d9902eea02aac700cf7
tygonz <tylergonzales3@gmail.com>
2025-09-06 00:29:01 -0700

    sort_dictionary and pretty print

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from stats import get_num_words, get_character_count
+from stats import get_num_words, get_character_count, sort_dictionary
+import sys
 
 def get_book_text(file_input):
     # open file
@@ -9,14 +10,28 @@ def get_book_text(file_input):
     return file_text
 
 def main():
-    text = get_book_text("books/frankenstein.txt")
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    # input_text = "books/frankenstein.txt"
+    input_text = sys.argv[1]
+    text = get_book_text(input_text)
     
     num_words = get_num_words(text)
-    print(f"{num_words} words found in the document")
+    # print(f"{num_words} words found in the document")
     
     num_characters = get_character_count(text)
-    print(num_characters)
+    # print(num_characters)
 
+    sorted_count = sort_dictionary(num_characters)
+
+    print(f"""============ BOOKBOT ============
+Analyxing book found at {input_text}...
+----------- Word Count ----------
+Found {num_words} total words
+--------- Character Count -------""")
+    for item in sorted_count:
+        print(f"{item["char"]}: {item["num"]}")
 
 if __name__ == "__main__":
     main()
--- stats.py
+++ stats.py
@@ -11,3 +11,18 @@ def get_character_count(text):
         else:
             character_counts[character] += 1
     return character_counts
+
+def return_number(unsorted_dict):
+    return unsorted_dict["num"]
+
+def sort_dictionary(input_dict):
+    new_list = []
+    for item in input_dict:
+        if not item.isalpha():
+            continue
+        dict_unit = {}
+        dict_unit["char"] = item
+        dict_unit["num"] = input_dict[item]
+        new_list.append(dict_unit)
+    new_list.sort(reverse=True, key=return_number)
+    return new_list
